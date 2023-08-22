06c203aa95a4504d3928c430cca4c8fbf6d78f91
Koosthzuien <>
2023-08-22 18:53:31 +0200

    Added new onSale and originalPrice properties to product datatype

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,8 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  onSale: boolean;
+  originalPrice?: number;
 }
 
 export const products: Product[] = [
@@ -10,18 +12,22 @@ export const products: Product[] = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    onSale: false
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    onSale: false
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    onSale: true,
+    originalPrice: 500
   }
 ];
