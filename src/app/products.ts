5ab6852d4c2a46853bd916fb73bce617faa4567b
Koosthzuien <>
2023-08-23 20:17:56 +0200

    Added image as new property for product

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  image: string;
 }
 
 export const products: Product[] = [
@@ -10,18 +11,21 @@ export const products: Product[] = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    image: "https://openmoji.org/data/color/svg/260E.svg"
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image: "https://openmoji.org/data/color/svg/1F4DE.svg"
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    image: "https://openmoji.org/data/color/svg/1F4F1.svg"
   }
 ];
