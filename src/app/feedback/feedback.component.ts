beeb8ff0751a7c60340c15e0420497b603478b0f
KyleO <koosthuizen@retrorabbit.co.za>
2024-01-09 09:09:03 +0200

    Added feedback modal

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef, ViewChild } from '@angular/core';
 import { FeedbackService } from '../feedback.service';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Feedback } from '../models/feedback';
+import { Modal, ModalOptions } from 'flowbite';
 
 @Component({
   selector: 'app-feedback',
@@ -15,6 +16,24 @@ public form: FormGroup = new FormGroup({
   feedback: new FormControl('')
 });
 
+ @ViewChild("feedbackModal")
+  modal!: ElementRef; 
+
+  // options with default values
+modalOptions: ModalOptions = {
+  placement: 'center',
+  backdrop: 'dynamic',
+  backdropClasses:
+      'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+  closable: false
+};
+
+// instance options object
+modalInstanceOptions = {
+id: 'feedback-modal',
+override: true
+};
+
   constructor(private feedbackService: FeedbackService) {
   }
 
@@ -22,7 +41,14 @@ public form: FormGroup = new FormGroup({
    const name = this.form.get('name')?.value;
    const feedbackType = this.form.get('feedbackType')?.value;
    const feedback = this.form.get('feedback')?.value;
+   const modal = new Modal(this.modal.nativeElement, this.modalOptions, this.modalInstanceOptions);
+   modal.show();
 
    this.feedbackService.sendFeedback({name, feedbackType, feedback});
+  }
+
+  hideModal() {
+    const modal = new Modal(this.modal.nativeElement, this.modalOptions, this.modalInstanceOptions);
+    modal.hide();
   }
 }
